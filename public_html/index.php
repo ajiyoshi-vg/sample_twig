f9381d2cd3a4f99c370584a433c28ad7e3226db5
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2013-02-27 12:11:01 +0900

    readme追加

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . "/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem(__DIR__ . "/templates");
+$loader = new Twig_Loader_Filesystem(__DIR__ . "/../templates");
 $twig = new Twig_Environment($loader, array('charset'=>'eucjp'));
 $template = $twig->loadTemplate("index.html");
 
